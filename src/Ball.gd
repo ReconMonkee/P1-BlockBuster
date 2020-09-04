5c4cf096b617c03ad8df8563a896bd22bbddb175
Logansummers18 <33754789+Logansummers18@users.noreply.github.com>
2020-09-04 11:22:36 -0400

    Arrow now points in direction of angle selected, and ball stops on floor

--- src/Ball.gd
+++ src/Ball.gd
@@ -2,15 +2,14 @@ extends RigidBody2D
 
 var strength = 0
 var xValue = 0
-var yValue = 0
+var yValue = -1
 var launchPushed = false
 var newXValue = 0
-var newYValue = 0
 
 
 func _process(delta):
 	if launchPushed:
-		var direction = Vector2(newXValue, newYValue).normalized()
+		var direction = Vector2(newXValue, yValue).normalized()
 		var velocity = direction * strength
 		apply_impulse(Vector2.ZERO, velocity)
 		launchPushed = false
@@ -24,9 +23,6 @@ func _on_Launch_pressed():
 func _on_XValue_text_changed(new_text):
 	xValue = float(new_text)
 
-func _on_YValue_text_changed(new_text):
-	yValue = float(new_text)
-
 
 func _on_Strength_text_changed(new_text):
 	strength = float(new_text)
@@ -35,10 +31,16 @@ func _on_Strength_text_changed(new_text):
 func _on_Ball_body_entered(body):
 	if body.is_in_group("blocks"):
 		body.queue_free()
-
+	if body.is_in_group("Floor"):
+		applied_force = Vector2(0, 0)
+		applied_torque = 0
+		linear_velocity = Vector2 (0,0)
+		angular_velocity = 0
 
 func _on_Set_pressed():
 	newXValue = xValue
-	newYValue = yValue
+	$CollisionShape2D.rotation_degrees = 0
+	$Arrow.rotation_degrees = 0
+	rotation_degrees = 0
 	$Arrow.show()
 	$Arrow.rotate(newXValue)
