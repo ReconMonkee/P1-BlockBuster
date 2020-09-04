5f5d7e2dcc2cf79ee9b480cedd076f4985a7b846
Logansummers18 <33754789+Logansummers18@users.noreply.github.com>
2020-09-03 21:28:24 -0400

    Add files via upload
    Added the arrow sprite

--- src/Ball.gd
+++ src/Ball.gd
@@ -4,10 +4,13 @@ var strength = 0
 var xValue = 0
 var yValue = 0
 var launchPushed = false
+var newXValue = 0
+var newYValue = 0
+
 
 func _process(delta):
 	if launchPushed:
-		var direction = Vector2(xValue, yValue).normalized()
+		var direction = Vector2(newXValue, newYValue).normalized()
 		var velocity = direction * strength
 		apply_impulse(Vector2.ZERO, velocity)
 		launchPushed = false
@@ -15,11 +18,11 @@ func _process(delta):
 
 func _on_Launch_pressed():
 	launchPushed = true
-	
+	$Arrow.hide()
+
 
 func _on_XValue_text_changed(new_text):
 	xValue = float(new_text)
-
 
 func _on_YValue_text_changed(new_text):
 	yValue = float(new_text)
@@ -32,3 +35,10 @@ func _on_Strength_text_changed(new_text):
 func _on_Ball_body_entered(body):
 	if body.is_in_group("blocks"):
 		body.queue_free()
+
+
+func _on_Set_pressed():
+	newXValue = xValue
+	newYValue = yValue
+	$Arrow.show()
+	$Arrow.rotate(newXValue)
